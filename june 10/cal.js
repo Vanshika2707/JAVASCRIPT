f5671c720bd1e396564fbbd8ee80ad7ee80c6ef3
vanshika gupta <vanshikagupta27072005@gmail.com>
2025-06-13 13:45:53 +0530

    new

--- june 10/cal.js
+++ june 10/cal.js
@@ -3,7 +3,7 @@ let display = document.getElementById('display');
 function appendValue(value) {
   display.value += value;
 }
-// for display
+// for display f
 function clearDisplay() {
   display.value = '';
 }
